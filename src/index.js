d5f4df44a580204259e981215b706973da00e907
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-03-27 12:10:28 +0300

    Updating startup files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const inputTxt = document.querySelector('#search-box');
+const ulCountry = document.querySelector('.country-list');
+const divCountry = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
